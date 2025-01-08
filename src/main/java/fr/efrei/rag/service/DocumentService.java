ce6c6e753acb1b1ceadead9bcbf250947c89db66
ARNDCLZD <aime5arnaud@gmail.com>
2025-01-08 17:01:17 +0100

    #9 create document architecture

--- src/main/java/fr/efrei/rag/service/DocumentService.java
+++ src/main/java/fr/efrei/rag/service/DocumentService.java
@@ -6,17 +6,34 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class DocumentService {
     private static final Logger log = LoggerFactory.getLogger(DocumentService.class);
     private final DocumentRepository documentRepository;
 
-    public DocumentService(DocumentRepository documentRepository){
+    public DocumentService(DocumentRepository documentRepository) {
         this.documentRepository = documentRepository;
     }
 
-    public Document buildAndSave(Document document){
-        log.debug("Request to buildAndSave Document : {}", document);
+    public Document buildAndSave(Document document) {
+        log.debug("Request to buildAndSave Document: {}", document);
         return documentRepository.save(document);
+    }
+
+    public List<Document> findAll() {
+        log.debug("Request to get all Documents");
+        return documentRepository.findAll();
+    }
+
+    public Document findById(Long id) {
+        log.debug("Request to get Document by ID: {}", id);
+        return documentRepository.findById(id).orElse(null);
+    }
+
+    public void deleteById(Long id) {
+        log.debug("Request to delete Document by ID: {}", id);
+        documentRepository.deleteById(id);
     }
 }
